ac0d90e12a682421509d0e3bd4f5de332241d04a
PC TOULOUSE <alumnotlsch@campustls.local>
2025-10-03 16:00:07 -0500

    ingreso de la pantalla GO y V

--- Assets/Scripts/Menu/GameManagerBotones.cs
+++ Assets/Scripts/Menu/GameManagerBotones.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 public class GameManagerBotones : MonoBehaviour
 {
     [SerializeField] GameObject menuPausa;
-    [SerializeField] PlayerInteraction player;
+    [HideInInspector] public PlayerInteraction player;
     //public int scene;
     private void Start()
     {
