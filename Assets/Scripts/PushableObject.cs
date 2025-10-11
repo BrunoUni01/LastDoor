ceabe4eb7ca6b2703b6eee5b75d07dbd1b2665a8
Bruno <brunogokue45@gmail.com>
2025-10-11 16:25:06 -0500

    Correccion de PushableObject falta un script pull

--- Assets/Scripts/PushableObject.cs
+++ Assets/Scripts/PushableObject.cs
@@ -6,29 +6,52 @@ public class PushableObject : MonoBehaviour
     private Validador valida;
     private Transform target;
     private Rigidbody2D rb;
+    private bool onCollision;
     [SerializeField] bool isPuzzleTres;
-    private void Awake() => rb = GetComponent<Rigidbody2D>();
+    private void Awake() { rb = GetComponent<Rigidbody2D>(); onCollision = false; }
     void Update()
     {
+        if (onCollision) return;
         if (target == null) return;
         transform.position = Vector2.MoveTowards(transform.position, target.position, Time.deltaTime);
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (!collision.collider.CompareTag("Player")) return;
+        onCollision = true;
         Vector2 direction = collision.contacts[0].normal;
         rb.MovePosition(rb.position + direction * speedMultiply * Time.fixedDeltaTime);
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Player")) return;
+        onCollision = false;
+        rb.constraints = RigidbodyConstraints2D.None;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (isPuzzleTres) return;
+        if (isPuzzleTres || onCollision) return;
         if (!collision.CompareTag("Finish")) return;
         rb.constraints = RigidbodyConstraints2D.FreezeAll;
         target = collision.transform;
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!isPuzzleTres) return;
+        if (!collision.CompareTag("Finish")) return;
+        target = null;
+    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (!collision.CompareTag("Player")) return;
+    //    Vector2 direction = collision.GetComponent<colldd>.contacts[0].normal;
+    //    rb.MovePosition(rb.position + direction * speedMultiply * Time.fixedDeltaTime);
+    //}
     public void Validador(Transform pos) 
     {
-        rb.constraints = RigidbodyConstraints2D.FreezeAll;
         target = pos.transform;
+        if (onCollision) return;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        
     }
 }
